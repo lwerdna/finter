f3b4717242e463d0ad2af6b9db622960f85a0276
Andrew Lamoureux <andrew@vector35.com>
2019-08-19 00:25:06 -0400

    new intervaltree -> hierarchy algorithm

--- helpers.py
+++ helpers.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import io
+import re
 import sys
 
 from finter import *
+from intervaltree import Interval, IntervalTree
 
 dissectors = [
     elf32.analyze,
@@ -33,4 +35,72 @@ def dissect_file(fpath):
     # cleanup, return
     buf.close()
     sys.stdout = old_stdout
-    return interval_lines
+    return interval_lines.split('\n')
+
+class hnode():
+    def __init__(self, interval):
+        self.interval = interval
+        self.children = []
+    def __str__(self, depth=0):
+        result = '  '*depth+'hnode'
+        result += str(self.interval) + '\n'
+        for c in sorted(self.children, key=lambda x: x.interval.begin):
+            result += c.__str__(depth+1)
+        return result
+
+#------------------------------------------------------------------------------
+# intervaltree stuff
+#------------------------------------------------------------------------------
+
+def intervals_from_text(lines):
+    """ convert list of "[0 5] blah" lines to intervals """
+
+    intervals = []
+    for (i,line) in enumerate(lines):
+        #print('line %d: %s' % (i,line))
+        if not line:
+            continue
+
+        m = re.match(r'\[(.*),(.*)\) (.*)', line)
+        if not m:
+            raise Exception('MALFORMED: %s' % line)
+
+        # Interval .begin .end .data
+        i = Interval(int(m.group(1),16), int(m.group(2),16), m.group(3))
+        intervals.append(i)
+
+    return intervals
+
+def interval_tree_to_hierarchy(tree):
+    """ convert IntervalTree to a hierarchy using hnode """
+
+    # initialize interval -> node mapping
+    child2parent = {i:None for i in tree}
+
+    # consider every interval a possible parent
+    for parent in tree:
+        # whatever intervals they envelop are their possible children
+        children = tree.envelop(parent)
+        children = list(filter(lambda c: c.length() != parent.length(), children))
+        for c in children:
+            # children without a parent are adopted immediate
+            if not child2parent[c]:
+                child2parent[c] = parent
+            # else children select their smallest parents
+            else:
+                child2parent[c] = min(child2parent[c], parent, key=lambda x: x.length())
+
+    # wrap the child2parent relationships into hnode
+    hnRoot = hnode(Interval(tree.begin(), tree.end(), "root"))
+    inter2hnode = { x:hnode(x) for x in tree }
+
+    for (child, parent) in child2parent.items():
+        hnChild = inter2hnode[child]
+        if not parent:
+            hnRoot.children.append(hnChild)
+        else:
+            hnParent = inter2hnode[parent]
+            hnParent.children.append(hnChild)
+
+    # done
+    return hnRoot
--- tree.py
+++ tree.py
@@ -5,52 +5,27 @@ import io
 import sys
 from finter import elf32, elf64
 from intervaltree import Interval, IntervalTree
-from helpers import dissect_file
+from helpers import dissect_file, intervals_from_text, interval_tree_to_hierarchy
 
-def print_recur(tree, a, depth=0):
+def print_recur(hnode, depth=0):
     indent = depth*'  '
 
-    length = a.end - a.begin
+    a = hnode.interval
+    length = a.length()
     lengthStr = '%d'%length if length < 16 else '0x%X'%length
     print('%08X: %s(%s) %s' % (a.begin, indent, lengthStr, a.data))
 
-    for child in sorted(tree.envelop(a)):
-        if a == child: continue
-        print_recur(tree, child, depth+1)
+    for child in sorted(hnode.children, key=lambda x: x.interval.begin):
+        print_recur(child, depth+1)
 
 if __name__ == '__main__':
-    interval_lines = dissect_file(sys.argv[1])
-    if not interval_lines:
+    lines = dissect_file(sys.argv[1])
+    if not lines:
         print('no file dissectors answered the call')
         sys.exit(-1)
 
-    intervals = []
-    for (i,line) in enumerate(interval_lines.split('\n')):
-        #print('line %d: %s' % (i,line))
-        if not line:
-            continue
-
-        m = re.match(r'\[(.*),(.*)\) (.*)', line)
-        if not m:
-            raise Exception('MALFORMED: %s' % line)
-
-        # Interval .begin .end .data
-        i = Interval(int(m.group(1),16), int(m.group(2),16), m.group(3))
-        intervals.append(i)
-
+    intervals = intervals_from_text(lines)
     tree = IntervalTree(intervals)
+    hnRoot = interval_tree_to_hierarchy(tree)
 
-    #print('top level intervals:')
-    enveloped = set()
-    for a in tree:
-        for b in tree.envelop(a):
-            if b != a:
-                enveloped.add(b)
-
-    standalone = []
-    for a in tree:
-        if not a in enveloped:
-            standalone.append(a)
-
-    for a in sorted(standalone):
-        print_recur(tree, a)
+    print_recur(hnRoot)
