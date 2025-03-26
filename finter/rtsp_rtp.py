424ee29fecdf037f3fc15004ade81325ce6ea133
Andrew Lamoureux <lwerdna@users.noreply.github.com>
2025-03-26 10:08:01 -0400

    Fix RTP: payload_type is 7, not 8 bits

--- finter/rtsp_rtp.py
+++ finter/rtsp_rtp.py
@@ -26,7 +26,7 @@ def seek_nalu(fp):
         sclen = 4
     if sclen is None:
         raise Exception(f'expected start code at offset 0x{home:X}')
-   
+
     # find next start code
     end = buffer.find(b'\x00\x00\x01', sclen)
     if end == -1:
@@ -40,7 +40,8 @@ def seek_nalu(fp):
     fp.seek(home, io.SEEK_SET)
     return (sclen, nalu_len)
 
-def tag_rtp(fp, length):
+
+def tag_rtp(fp, length, channel=None):
     start = fp.tell()
 
     # RTP HEADER (no extension)
@@ -62,14 +63,23 @@ def tag_rtp(fp, length):
     # 16: extensions    (4*extension_len)
     # ??: payload       (?)
 
-    flags = tagUint8(fp, 'flags')
-    ptype = tagUint8(fp, 'payload_type')
+    tmp = uint8(fp, peek=True)
+    ver = tmp >> 6
+    P = (tmp >> 5) & 1
+    X = (tmp >> 4) & 1
+    CC = tmp & 0xf
+    tagUint8(fp, 'b0', f'ver={ver} pad={P} ext={X} CC={CC}')
+
+    tmp = uint8(fp, peek=True)
+    M = tmp >> 7
+    payload_type = tmp & 0x7f
+    tagUint8(fp, 'b1', f'M={M} payload_type={payload_type}')
     tagUint16(fp, 'sequence_num')
     tagUint32(fp, 'timestamp')
     tagUint32(fp, 'sync')
 
     # extensions?
-    if flags & 0x10:
+    if X:
         extension_id = tagUint16(fp, 'extension_id')
         extension_len = tagUint16(fp, 'extension_len')
         for i in range(extension_len):
@@ -80,15 +90,17 @@ def tag_rtp(fp, length):
 
     # is padding present?
     padlen = 0
-    if flags & 0x20:
+    if P:
         tmp = fp.tell()
         fp.seek(payload_length - 1, io.SEEK_CUR)
         padlen = uint8(fp)
         assert padlen in [1, 2, 3]
         payload_length -= padlen
         fp.seek(tmp)
-    
-    tag(fp, payload_length, 'payload')
+
+    tag(fp, payload_length, 'payload', '', peek=True)
+    h264.tag_nalu(fp, payload_length)
+
     if padlen:
         tag(fp, padlen, 'padding')
 
@@ -101,13 +113,13 @@ def tag_rtsp(fp):
 
     if (magic := tagUint8(fp, 'magic')) != 0x24:
         raise Exception(f'got magic byte: 0x{magic:X} (expected 0x24)')
-    tagUint8(fp, 'channel')
+    channel = tagUint8(fp, 'channel')
     payload_len = tagUint16(fp, 'payload_len')
 
     if payload_len:
         used = 0
         while used < payload_len:
-            used += tag_rtp(fp, payload_len)
+            used += tag_rtp(fp, payload_len, channel)
 
     tagFromPosition(fp, start, 'RTSP')
 
