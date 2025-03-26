05616183dc140a7c9dcf7e115705580092c11763
Andrew Lamoureux <lwerdna@users.noreply.github.com>
2025-03-26 11:29:19 -0400

    Make seek_nalu() more efficient

--- finter/h264.py
+++ finter/h264.py
@@ -46,35 +46,51 @@ def nalu_type_to_str(x):
         case 31: return 'reserved'
         case _: return '(unknown)'
 
-# assumes fp is positioned at a start code
-# returns (start_code_len, nalu_len)
-# very inefficient, does large reads to the end of the file from each start code
+# +------------+ <- o_start
+# | start code |
+# +------------+ <- o_nalu
+# | NALU       |
+# +------------+ <- o_end
+# | start code |
+# +------------+
 def seek_nalu(fp):
-    home = fp.tell()
+    o_code = fp.tell()
 
-    buffer = fp.read()
+    buffer = fp.read(4)
 
     # read start code
-    sclen = None
     if buffer[0:3] == b'\x00\x00\x01':
-        sclen = 3
+        code = b'\x00\x00\x01'
     elif buffer[0:4] ==  b'\x00\x00\x00\x01':
-        sclen = 4
-    if sclen is None:
-        raise Exception(f'expected start code at offset 0x{home:X}')
-
-    # find next start code
-    end = buffer.find(b'\x00\x00\x01', sclen)
-    if end == -1:
-        nalu_len = len(buffer) - sclen
+        code = b'\x00\x00\x00\x01'
     else:
-        if buffer[end-1] == 0:
-            end -= 1
-        nalu_len = end - sclen
+        raise Exception(f'expected start code at 0x{o_code:X}')
+
+    o_nalu = o_end = o_code + len(code)
+    fp.seek(o_nalu)
+
+    while True:
+        chunk = fp.read(4096)
+
+        # found! end is offset within buffer
+        i = chunk.find(code)
+        if i != -1:
+            o_end += i
+            break
+
+        o_end += len(chunk)
+
+        # did we reach end of file?
+        if len(chunk) < 4096:
+            break
+
+        # back
+        o_end -= len(code)
+        fp.seek(-len(code), io.SEEK_CUR)
 
     # back home
-    fp.seek(home, io.SEEK_SET)
-    return (sclen, nalu_len)
+    fp.seek(o_code, io.SEEK_SET)
+    return (len(code), o_end - o_nalu)
 
 def tag_nalu_fu_a(fp, length):
     b0 = int.from_bytes(tag(fp, 1, 'fu_indicator', ''), 'big')
--- finter/rtsp_rtp.py
+++ finter/rtsp_rtp.py
@@ -10,37 +10,6 @@ from .helpers import *
 
 from . import h264
 
-# assumes fp is positioned at a start code
-# returns (start_code_len, nalu_len)
-# very inefficient, does large reads to the end of the file from each start code
-def seek_nalu(fp):
-    home = fp.tell()
-
-    buffer = fp.read()
-
-    # read start code
-    sclen = None
-    if buffer[0:3] == b'\x00\x00\x01':
-        sclen = 3
-    elif buffer[0:4] ==  b'\x00\x00\x00\x01':
-        sclen = 4
-    if sclen is None:
-        raise Exception(f'expected start code at offset 0x{home:X}')
-
-    # find next start code
-    end = buffer.find(b'\x00\x00\x01', sclen)
-    if end == -1:
-        nalu_len = len(buffer) - sclen
-    else:
-        if buffer[end-1] == 0:
-            end -= 1
-        nalu_len = end - sclen
-
-    # back home
-    fp.seek(home, io.SEEK_SET)
-    return (sclen, nalu_len)
-
-
 def tag_rtp(fp, length, channel=None):
     start = fp.tell()
 
